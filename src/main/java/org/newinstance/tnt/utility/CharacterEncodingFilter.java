e8c739f9af239f167b3c7cb6993756b48929a90d
Marc Walter <m.walter@ymail.com>
2013-01-09 21:22:35 +0100

    filter added to correct character encoding problem of PrimeFaces

---
+++ src/main/java/org/newinstance/tnt/utility/CharacterEncodingFilter.java
@@ -0,0 +1,48 @@
+/*
+ * TnT - Things and tasks to do
+ * Licenced under General Public Licence v3 (GPLv3)
+ * newInstance.org, 2012-2013
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.newinstance.tnt.utility;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * Corrects the special characters parsing problem caused by PrimeFaces by setting the right character encoding.
+ *
+ * @see <a href="http://stackoverflow.com/questions/10721342/utf-8-form-submit-in-jsf-is-corrupting-data">PrimeFaces encoding problem</a>
+ * @author mwalter
+ */
+public class CharacterEncodingFilter implements Filter {
+
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
+
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
