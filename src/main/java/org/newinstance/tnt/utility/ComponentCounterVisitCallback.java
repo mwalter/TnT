2c21706e432ce1b421b8fc17071f970ca44ca0a3
Marc Walter <mwalter@newinstance.org>
2014-01-18 22:51:21 +0100

    added support to count number of components in faces context

---
+++ src/main/java/org/newinstance/tnt/utility/ComponentCounterVisitCallback.java
@@ -0,0 +1,52 @@
+/*
+ * TnT - Things and tasks to do
+ * Licensed under General Public License v3 (GPLv3)
+ * newInstance.org, 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.newinstance.tnt.utility;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.visit.VisitCallback;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitResult;
+
+/**
+ * Implements the visit callback to count all components in the context.
+ *
+ * @author mwalter
+ */
+public class ComponentCounterVisitCallback implements VisitCallback {
+
+    /** Counter for components. */
+    private int counter = 0;
+
+    @Override
+    public VisitResult visit(final VisitContext context, final UIComponent target) {
+        counter++;
+        return VisitResult.ACCEPT;
+    }
+
+    /**
+     * Returns the counter.
+     *
+     * @return the counter
+     */
+    public int getCount() {
+        return counter;
+    }
+
+}
